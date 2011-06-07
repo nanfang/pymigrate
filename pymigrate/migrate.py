6c8dceca1117527beef6c1aa4779ce549576fce9
nanfang <nanfang@localhost.(none)>
2011-06-07 23:17:04 +0800

    add skeleton of the program

---
+++ pymigrate/migrate.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals, print_function, division
+import glob
+import os
+import sys
+
+def migrate(script_path, migrate_trace=_default_migrate_trace):
+    print('Begin to migrate ...')
+    migrate_trace.init()
+    migrate_trace.clear_last_error()
+
+    all_scripts = [script for script in glob.glob(os.path.join(script_path, '*.*'))]
+    all_scripts.sort()
+    for script in all_scripts:
+        if script not in migrate_trace.executed():
+            try:
+                migrate_trace.run(script)
+            except Exception as ex:
+                migrate_trace.error(script, ex.message)
+                raise
+    print('System is up to date')
+
+
+class MigrateTrace(object):
+
+    # make sure migrate tracing storage are ready
+    def init(self):
+        pass
+    
+    def clear_last_error(self):
+        pass
+
+    def run(self, script):
+        pass
+
+    def error(self, script, err):
+        pass
+
+
+class FileMigrateTrace(MigrateTrace):
+    pass
+
+_default_migrate_trace= FileMigrateTrace()
+
+
+if __name__ == '__main__':
+    migrate(sys.argv[1])
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="py-migrate",
+      version="0.1",
+      description="Data migration tools written by python",
+      author="Fang Nan",
+      url="https://github.com/nanfang",
+      packages=find_packages(),
+)
