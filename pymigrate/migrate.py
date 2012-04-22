98dee6ab923be30292f82132ee009d71d61c586f
Fang Nan <nanfang05@gmail.com>
2012-04-22 23:25:18 +0800

    complete coding for v0.1

--- pymigrate/migrate.py
+++ pymigrate/migrate.py
@@ -1,63 +1,8 @@
 import glob
-import json
 import os
 import sys
 import optparse
 import json
-
-class MigrateTrace(object):
-    # make sure migrate tracing storage are ready
-    def init(self):
-        raise NotImplementedError()
-
-    def clear_last_error(self):
-        raise NotImplementedError()
-
-    def run(self, script):
-        raise NotImplementedError()
-
-    def error(self, script, err):
-        raise NotImplementedError()
-
-
-class FileMigrateTrace(MigrateTrace):
-    def init(self):
-        # TODO load migrate trace from .pymigrate file
-        pass
-
-    def clear_last_error(self):
-        # TODO ?
-        pass
-
-    def run(self, script):
-        # TODO just run the script, and save the migrate trace
-        os.system(script)
-        pass
-
-    def error(self, script, err):
-        # TODO print error and break migrate
-        pass
-
-_default_migrate_trace= FileMigrateTrace()
-
-
-def migrate(script_path, migrate_trace=_default_migrate_trace):
-    print('Begin to migrate ...')
-    migrate_trace.init()
-    migrate_trace.clear_last_error()
-
-    all_scripts = [script for script in glob.glob(os.path.join(script_path, '*.*'))]
-    # TODO sort by index
-    all_scripts.sort()
-    for script in all_scripts:
-        if script not in migrate_trace.executed():
-            try:
-                migrate_trace.run(script)
-            except Exception as ex:
-                migrate_trace.error(script, ex.message)
-                raise
-    print('System is up to date')
-
 
 class Migrator(object):
     def __init__(self, directory):
@@ -75,24 +20,75 @@ class Migrator(object):
             print(' %s' % script)
 
     def clear_trace(self):
-        pass
+        trace_file = self.trace_file()
+        if os.path.exists(trace_file):
+            with open(trace_file, 'w') as fp:
+                json.dump([], fp)
+        print('All migrate trace cleared.')
+
 
     def run(self):
-        pass
+        if not self.to_execute:
+            print('Your system is up to date.')
+            return
+        for script in self.to_execute:
+            print('Executing: %s' % script)
+            try:
+                if script.endswith('.py'):
+                    status = os.system('python %s' % os.path.join(self.directory, script))
+                else:
+                    status = os.system(os.path.join(self.directory, script))
+            except Exception as ex:
+                print('Fail to execute %s because: %s' %(script, ex))
+                self.exit_run(-1)
+            if status:
+                print('Fail to execute %s' % script)
+                self.exit_run(status)
+            self.executed.append(script)
+        self.exit_run(0)
+
+    def exit_run(self, status):
+        with open(self.trace_file(), 'w') as fp:
+            json.dump(self.executed, fp)
+        sys.exit(status)
+
+    def trace_file(self):
+        return '%s/.pymigrate' % (os.getenv('USERPROFILE') or os.getenv('HOME'))
 
     def load_trace(self):
-        home = os.getenv('USERPROFILE') or os.getenv('HOME')
-        trace_file = '%s/.pymigrate' % home
+        trace_file = self.trace_file()
         if not os.path.exists(trace_file):
             self.executed = []
             with open(trace_file, 'w') as fp:
-                json.dump({}, fp)
+                json.dump([], fp)
         else:
             with open(trace_file, 'r') as fp:
-                self.executed = json.load(fp).get(self.directory, [])
+                self.executed = json.load(fp)
 
         self.to_execute = [os.path.basename(path) for path in glob.glob(os.path.join(self.directory, '*'))
-                      if os.path.isfile(path) and os.path.basename(path) not in self.executed]
+                           if os.path.isfile(path) and os.path.basename(path) not in self.executed]
+        self.to_execute.sort(cmp=_sort_script)
+
+
+def _sort_script(name1, name2):
+    index1 = _script_index(name1)
+    index2 = _script_index(name2)
+    if index1 == index2:
+        return cmp(name1, name2)
+    if index1 is not None and index2 is not None:
+        return index1 - index2
+    if index1 is not None:
+        return 1
+    if index2 is not None:
+        return -1
+
+
+def _script_index(name):
+    pos = name.find('_')
+
+    if pos > 0 and name[:pos].isdigit():
+        return int(name[:pos])
+    return None
 
 
 def _option_parser():
---
+++ tests/scripts/0002_migrate2.py
@@ -0,0 +1,6 @@
+#! /urs/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division, unicode_literals, print_function
+
+if __name__ == '__main__':
+    print('test2')
